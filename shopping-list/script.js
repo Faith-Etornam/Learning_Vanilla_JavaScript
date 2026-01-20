c189a6ca46fb5c88d0d7f17e51c23fc1772c2dbc
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-20 11:50:03 +0000

    Shopping list

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,20 +1,30 @@
-function createNewItem(item) {
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(item))
-
+function createButton(classes) {
   const button = document.createElement("button");
-  button.className = "remove-item btn-link text-red";
-
-  const icon = document.createElement("i");
-  icon.className = "fa-solid fa-xmark";
-
-  button.appendChild(icon)
-  li.appendChild(button)
-
-  const items = document.querySelector('.items')
-  items.appendChild(li)
+  button.className = classes;
+  return button;
 }
 
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+function createNewItem(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton("remove-item btn-link text-red");
+  const icon = createIcon("fa-solid fa-xmark");
+
+  button.appendChild(icon);
+  li.appendChild(button);
+
+  const items = document.querySelector(".items");
+  items.appendChild(li);
+}
+
+createNewItem("eggs");
 
 // const itemForm = document.getElementById('item-form');
 // const itemInput = document.getElementById('item-input');
